0f1ded4073fdf366b45bd5108c253f9724b71784
Barsius <barsius78@gmail.com>
2020-03-07 21:47:03 +0200

    Game without money implemented

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Game {
     Deck deck = new Deck();
@@ -7,24 +8,29 @@ public class Game {
     Player player = new Player(deck, random);
 
     Game() {
+        Scanner scanner = new Scanner(System.in);
         while (true) {
-            dealer.firstMove();
-            player.firstMove();
-            player.play();
-            if (player.getPoints() < 21) {
-                dealer.play();
-                if (dealer.getPoints() < 21) {
-                    if (player.getPoints() > dealer.getPoints()) System.out.println("Player wins");
-                    else if (player.getPoints() < dealer.getPoints()) System.out.println("Dealer wins");
-                    else System.out.println("Draw");
-                } else System.out.println("Player wins");
-            } else if (player.getPoints() == 21) {
-                if (dealer.getPoints() == 21) {
-                    System.out.println("Draw");
-                } else System.out.println("Player wins");
-            } else System.out.println("Player busted");
-            player.resetPoints();
-            dealer.resetPoints();
+            System.out.println("Enter 1 if you want to continue");
+            if (scanner.nextInt() == 1) {
+                dealer.firstMove();
+                player.firstMove();
+                player.play();
+                if (player.getPoints() < 21) {
+                    dealer.play();
+                    if (dealer.getPoints() < 21) {
+                        if (player.getPoints() > dealer.getPoints()) System.out.println("Player wins");
+                        else if (player.getPoints() < dealer.getPoints()) System.out.println("Dealer wins");
+                        else System.out.println("Draw");
+                    } else System.out.println("Player wins");
+                } else if (player.getPoints() == 21) {
+                    if (dealer.getPoints() == 21) {
+                        System.out.println("Draw");
+                    } else System.out.println("Player wins");
+                } else System.out.println("Player busted");
+                player.resetPoints();
+                dealer.resetPoints();
+            }
+            else return;
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,6 @@
-import java.util.Random;
-
 public class Main {
 
     public static void main(String[] args){
-
-
         Game game = new Game();
-
-        //Player player = new Player();
-
-        /*
-        Deck deck = new Deck();
-        Random random = new Random();
-
-        Dealer dealer = new Dealer(deck, random);
-        dealer.firstMove();
-        dealer.play();
-         */
     }
 }
