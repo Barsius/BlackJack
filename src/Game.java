0887a9a8987b944e2661feafca0dec951a021783
Barsius <barsius78@gmail.com>
2020-03-08 18:09:12 +0200

    Slightly optimized class "Game"

--- src/Game.java
+++ src/Game.java
@@ -6,29 +6,21 @@ public class Game {
     Random random = new Random();
     Dealer dealer = new Dealer(deck, random);
     Player player = new Player(deck, random);
+    Scanner scanner = new Scanner(System.in);
 
     Game() {
-        Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("Enter 1 if you want to continue");
-            if (scanner.nextInt() == 1) {
-                dealer.firstMove();
-                player.firstMove();
-                BlackJack();
-                player.play();
-                dealer.play();
-                int pp = player.getPoints();
-                int dp = dealer.getPoints();
-                if(pp <= 21 & (pp > dp || dp > 21)) System.out.println("Player wins");
-                else if(pp == dp && pp < 21) System.out.println("Draw");
-                else System.out.println("Player lose");
-                reset();
+            if (scanner.nextInt() == 1){
+                if (!BlackJack()) RegularGame();
             }
             else return;
         }
     }
 
-    void BlackJack(){
+    boolean BlackJack(){
+        dealer.firstMove();
+        player.firstMove();
         if (player.getPoints() == 21){
             System.out.println("Player have a BlackJack");
             dealer.firstMove();
@@ -39,12 +31,19 @@ public class Game {
                 System.out.println("Player win with BlackJack");
             }
             reset();
-            continue;
-        }
+            return true;
+        } else return false;
     }
 
     void RegularGame(){
-
+            player.play();
+            dealer.play();
+            int pp = player.getPoints();
+            int dp = dealer.getPoints();
+            if(pp <= 21 & (pp > dp || dp > 21)) System.out.println("Player wins");
+            else if(pp == dp && pp < 21) System.out.println("Draw");
+            else System.out.println("Player lose");
+            reset();
     }
 
     void reset(){
